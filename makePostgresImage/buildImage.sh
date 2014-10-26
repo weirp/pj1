7026c83dfc1637383d7fc84dfbcd7c7b6285e2c8
Philip J Weir <weirp@acm.org>
2014-10-26 21:42:40 +0800

    push image to docker repo

--- makePostgresImage/buildImage.sh
+++ makePostgresImage/buildImage.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
-sudo docker build -t pjw_postgresql .
+sudo docker build -t weirp/pg1 .
 
 echo run it like this:
-echo sudo docker run --rm -P --name pg_test pjw_postgresql
+echo sudo docker run --rm -P --name pg1 pg1
 echo .
 echo link data like this:
-echo sudo docker run --rm -t -i --link pg_test:pg pjw_postgresql bash
+echo sudo docker run --rm -t -i --link pg_test:pg pg1 bash
 echo .
 echo to connect:
 echo $ sudo docker ps
 echo psql -h localhost -p 49153 -d docker -U docker --password
 echo .
 echo to save changes:
-echo docker commit pjw_postgresql
+echo docker commit pg1
+echo .
+echo to push to the repository:
+echo docker push weirp/pg1
 
 
